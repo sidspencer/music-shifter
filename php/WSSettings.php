d5874c0fce7ecb801315fa7afc8c522e4800977b
Sid Spencer <dis@bub.local>
2016-08-29 16:28:09 -0700

    Creation of new EQs upon POST.

--- php/WSSettings.php
+++ php/WSSettings.php
@@ -1,41 +1,67 @@
 <?php
 // Include confi.php
-include_once('config.php');
- 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    // Get data
-    $name = isset($_POST['name']) ? mysql_real_escape_string($_POST['name']) : "";
-    $email = isset($_POST['email']) ? mysql_real_escape_string($_POST['email']) : "";
-    $password = isset($_POST['pwd']) ? mysql_real_escape_string($_POST['pwd']) : "";
-    $status = isset($_POST['status']) ? mysql_real_escape_string($_POST['status']) : "";
-    
-    // Insert data into data base
-    $sql = "INSERT INTO `tuts_rest`.`users` (`ID`, `name`, `email`, `password`, `status`) VALUES (NULL, '$name', '$email', '$password', '$status');";
-    $result = mysql_query($sql);
+$conn = mysql_connect("localhost", "root", "");
+mysql_select_db('MusicShifter', $conn);
 
-    if($qur){
-        $json = array("status" => 1, "msg" => "Success adding user");
-    }else{
-        $json = array("status" => 0, "msg" => "Error adding user");
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    // Get EQ data from the POST
+    $name = isset($_POST['name']) ? 
+        mysql_real_escape_string($_POST['name']) : 
+        null;
+
+    $playbackRate = isset($_POST['playbackRate']) ? 
+        mysql_real_escape_string($_POST['playbackRate']) : 
+        null;
+
+    $trebleLevel = isset($_POST['trebleLevel']) ? 
+        mysql_real_escape_string($_POST['trebleLevel']) : 
+        null;
+    
+    $midLevel = isset($_POST['midLevel']) ? 
+        mysql_real_escape_string($_POST['midLevel']) : 
+        null;
+
+    $bassLevel = isset($_POST['bassLevel']) ? 
+        mysql_real_escape_string($_POST['bassLevel']) : 
+        null;
+
+    // Insert new EQ if we were passed all the settings.
+    if (is_null($name) || $is_null($playbackRate) || is_null($trebleLevel) || is_null($midLevel) || is_null($bassLevel)) {
+        $result = false;
+    }
+    else {
+        $sql = "INSERT INTO MusicShifter.EQ(name, playbackRate, trebleLevel, midLevel, bassLevel)" +
+            "VALUES ('$name', $playbackRate, $trebleLevel, $midLevel, $bassLevel);";
+        $result = mysql_query($sql);
+    }
+
+    if ($result) {
+        $jsonArr = array("status" => 1, "msg" => "Success adding user");
+    }
+    else {
+        $jsonArr = array("status" => 0, "msg" => "Error adding user");
     }
 }
 elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
-    // GET ALL
+    // Retrieve all EQs from the database.
     $sql = "SELECT id, name, playbackRate, trebleLevel, midLevel, bassLevel FROM EQ ORDER BY name ASC";
 
     $result = mysql_query($sql);
 
     if ($result) {
-        $json = $result->fetch_array(MYSQLI_ASSOC);
+        $jsonArr = $result->fetch_array(MYSQLI_ASSOC);
+    }
+    else {
+        $jsonArr = array("status" => 1, "msg" => "Error retrieving EQs");
     }
 }
 else {
-        $json = array("status" => 0, "msg" => "Request method not accepted");
+        $jsonArr = array("status" => 0, "msg" => "Request method not accepted");
 }
 
 @mysql_close($conn);
  
-/* Output header */
- header('Content-type: application/json');
- echo json_encode($json);
+// Output
+header('Content-type: application/json');
+echo json_encode($json);
 ?>
