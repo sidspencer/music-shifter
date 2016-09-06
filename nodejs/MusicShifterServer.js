e5ccb8db6e28dad1cea0aba53cea24e6c59ce4c9
Sid Spencer <dis@bub.local>
2016-09-06 12:25:53 -0700

    Incremental update. Send better mock data.

--- nodejs/MusicShifterServer.js
+++ nodejs/MusicShifterServer.js
@@ -1,18 +1,38 @@
+// Imports and constants.
 var http = require('http');
 
 var hostname = '127.0.0.1';
 var port = 3003;
 
-var server = http.createServer(function (request, response) {
-      response.writeHead(200, 
-            {
-                  "Content-Type": "text/plain",
-                  "Access-Control-Allow-Origin": "*"
-            }
-      );
+var mockEq = { "id": 1, "name": "mockEq", "playbackRate": 3, "trebleLevel": 3, "midLevel": 3, "bassLevel": 3 };
 
-      response.end('[ { "id": 1, "name": "bob", "playbackRate": 3, "trebleLevel": 3, "midLevel": 3, "bassLevel": 3 } ]');
+
+// Service
+var service = {
+      eqs: [mockEq]
+};
+
+// Utils
+var utils = {
+      writeHeadWithStatus: function writeHeadWithStatus(response, statusCode) {
+                  response.writeHead(statusCode, 
+                  {
+                        "Content-Type": "text/json",
+                        "Access-Control-Allow-Origin": "*"
+                  }
+            );
+      }
+};
+
+// Create Server
+var server = http.createServer(function (request, response) {
+      if (request.method == "GET") {
+            utils.writeHeadWithStatus(response, 200);
+            response.end(JSON.stringify(service.eqs));
+      }
 });
+
+
 
 server.listen(port);
 
