2d4f17fdbfb532f07b2684e222057f9703b9eb0d
Sid Spencer <dis@bub.local>
2016-09-06 13:20:27 -0700

    Add CRUD operations with REST verbs.

--- nodejs/MusicShifterServer.js
+++ nodejs/MusicShifterServer.js
@@ -5,7 +5,7 @@ var hostname = '127.0.0.1';
 var port = 3003;
 
 var mockEq = { 
-      "id": 1, 
+      "id": 0, 
       "name": "mockEq", 
       "playbackRate": 3, 
       "trebleLevel": 3, 
@@ -16,7 +16,57 @@ var mockEq = {
 
 // Service
 var service = {
-      eqs: [mockEq]
+      eqs: [mockEq],
+      nextEqId: 1,
+
+      getEqs: function() {
+            return this.eqs;
+      },
+      setEqs: function(eqs) {
+            this.eqs = eqs;
+            return this.eqs;
+      },
+      createEq: function addEq(eq) {
+            eq.id = nextEqId;
+            nextEqId++;
+
+            this.eqs.push(eq);
+
+            return eq.id;
+      },
+      getEqById: function getEqById(eqId) {
+          for (var i = 0; i < this.eqs.length; i++) {
+                if (this.eqs[i].id == eqId) {
+                      return this.eqs[i];
+                }
+          }
+
+          // Failure case
+          return undefined;
+      },
+      updateEq: function updateEq(eq) {
+            for (var i = 0; i < this.eqs.length; i++) {
+                  if (this.eqs[i].id == eq.id) {
+                        this.eqs[i] = eq;
+                        return eq;
+                  }
+            }
+
+            // Failure case
+            return undefined;
+      },
+      deleteEq: function deleteEq(eq) {
+            for (var i = 0; i < this.eqs.length; i++) {
+                  if (this.eqs[i].id == eq.id) {
+                        this.eqs.splice(i);
+                        return true;
+                  }
+            }
+
+            // Failure case
+            return false;
+      }
+
 };
 
 // Utils
@@ -28,14 +78,72 @@ var utils = {
                         "Access-Control-Allow-Origin": "*"
                   }
             );
+      },
+      writeErrorStatusWithMessage: function writeErrorStatusWithMessage(response, message) {
+            this.writeHeadWithStatus(response, 500);
+            response.end(JSON.parse({
+                  error: message
+            }))
       }
 };
 
 // Create Server
 var server = http.createServer(function (request, response) {
+      if (!request.url.match("/:" + port + "\/eqs\//")) {
+            utils.writeErrorStatusWithMessage(response, "No such service.");
+      }
+
+      if (request.url.match(/eqs\/eq)/)) {
+            if (request.method == "PUT") {
+                  try {
+                        var selectedEq = JSON.parse(response.body);
+
+                        if ((selectedEq = service.updateEq(selectedEq)) == undefined) {
+                              utils.writeErrorStatusWithMessage(response, "No such EQ.");
+                        } 
+                        else {
+                              utils.writeHeadWithStatus(response, 200);
+                              response.end(JSON.stringify(selectedEq));
+                        }
+                  }
+                  catch (ex) {
+                        utils.writeErrorStatusWithMessage(response, "Invalid data format.");
+                  }
+            }
+      }
+
       if (request.method == "GET") {
             utils.writeHeadWithStatus(response, 200);
-            response.end(JSON.stringify(service.eqs));
+            response.end(JSON.stringify(service.getEqs()));
+      }
+      else if (request.method == "PUT") {
+            try {
+                  service.setEqs(JSON.parse(response.body));
+                  utils.writeHeadWithStatus(response, 200);
+                  response.end((response.body));
+            }
+            catch (ex) {
+                  utils.writeErrorStatusWithMessage(response, "Invalid data format.")
+            }
+      }
+      else if (request.method == "POST") {
+            try {
+                  var newEq = JSON.parse(response.body);
+
+                  if ((newEq = this.createEq(newEq)) == undefined) {
+                        utils.writeErrorStatusWithMessage(response, "Error creating new EQ.");
+                  }
+                  else {
+                        utils.writeHeadWithStatus(response, 200);
+                        response.end(JSON.stringify(newEq));
+                  }
+            }
+            catch (ex) {
+                  utils.writeErrorStatusWithMessage(response, "Invalid data format.");
+            }
+      }
+      else if (request.method == "DELETE") {
+            service.setEqs({});
       }
 });
 
